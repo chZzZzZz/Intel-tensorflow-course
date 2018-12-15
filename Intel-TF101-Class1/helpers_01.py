834cd7ef9e88c92c5ccbb89da1dce0ae5078afa8
unknown <18829354152@163.com>
2018-12-15 17:40:17 +0800

    class1 intro to tf

---
+++ Intel-TF101-Class1/helpers_01.py
@@ -0,0 +1,13 @@
+import os.path as osp
+import itertools
+
+def get_fresh_dir(parent_dir):
+    ''' get an unused directory name in parent_dir '''
+    import itertools as it
+    import os.path as osp
+    for i in it.count():
+        possible_dir = osp.join(parent_dir, str(i))
+        if not osp.exists(possible_dir):
+            return possible_dir
+            
+get_fresh_dir('tbout/test')
